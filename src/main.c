297f223ed2f774707011ebf522086caaec500733
Maciej <maciejlasz@wp.pl>
2018-04-16 18:26:14 +0200

    commit

--- src/main.c
+++ src/main.c
@@ -16,6 +16,11 @@
 #include "GPIO.h"
 
 double temp=0;
+
+int16_t X;
+int16_t Y;
+int16_t Z;
+
 int main(void)
 {
 	initGPIODiodes();
@@ -34,25 +39,10 @@ int main(void)
 
 		TM_LIS302DL_LIS3DSH_ReadAxes(&Axes_Data);
 		temp = 2.3 / 127 * Axes_Data.Z * 10;
-		if (temp<=-5) {
-			GPIO_SetBits(GPIOD, GPIO_Pin_12);
-		} else {
-			GPIO_ResetBits(GPIOD, GPIO_Pin_12);
-		}
-		if (temp > -5 && temp <= 0) {
-			GPIO_SetBits(GPIOD, GPIO_Pin_13);
-		} else {
-			GPIO_ResetBits(GPIOD, GPIO_Pin_13);
-		}
-		if (temp > 0 && temp < 5) {
-			GPIO_SetBits(GPIOD, GPIO_Pin_14);
-		} else {
-			GPIO_ResetBits(GPIOD, GPIO_Pin_14);
-		}
-		if (temp >= 5) {
-			GPIO_SetBits(GPIOD, GPIO_Pin_15);
-		} else {
-			GPIO_ResetBits(GPIOD, GPIO_Pin_15);
-		}
+
+		X = Axes_Data.X;
+		Y = Axes_Data.Y;
+		Z = Axes_Data.Z;
+
 	}
 }
